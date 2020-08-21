b666232e237515389f9553a48f36d30fe13281e6
j3ffh95 <j3ffh95@gmail.com>
2020-08-21 12:38:28 -0400

    fetchPokemons function

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const poke_container = document.querySelector("#poke_container");
 const pokemons_number = 150;
 
+fetchPokemons();
+
+const fetchPokemons = async () => {
+  for (let i = 0; i <= pokemons_number; i++) {
+    await getPokemon(i);
+  }
+};
+
 const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`,
     res = await fetch(url),
     pokemon = await res.json();
 };
-
-getPokemon(1);
