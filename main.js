7c7404d292f3c53e407bcc992d5d075cc93f9af4
j3ffh95 <j3ffh95@gmail.com>
2020-08-21 12:51:41 -0400

    created colors object

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const poke_container = document.querySelector("#poke_container");
 const pokemons_number = 150;
 
-fetchPokemons();
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
 
 const fetchPokemons = async () => {
-  for (let i = 0; i <= pokemons_number; i++) {
+  for (let i = 1; i <= pokemons_number; i++) {
     await getPokemon(i);
   }
 };
@@ -13,4 +28,22 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`,
     res = await fetch(url),
     pokemon = await res.json();
+  createPokemonCard(pokemon);
 };
+
+fetchPokemons();
+
+function createPokemonCard(pokemon) {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+  const pokeInnerHTML = `
+
+  `;
+
+  pokemonEl.innerHTML = pokeInnerHTML;
+
+  poke_container.appendChild(pokemonEl);
+}
