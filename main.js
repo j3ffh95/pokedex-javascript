f35783711c9d7e5aa270cf1acaddd85ddcc989f3
j3ffh95 <j3ffh95@gmail.com>
2020-08-21 13:04:39 -0400

    Finished with the createPokemonCard function

--- main.js
+++ main.js
@@ -18,6 +18,8 @@ const colors = {
   normal: "#F5F5F5",
 };
 
+const main_types = Object.keys(colors);
+
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokemons_number; i++) {
     await getPokemon(i);
@@ -37,10 +39,20 @@ function createPokemonCard(pokemon) {
   const pokemonEl = document.createElement("div");
   pokemonEl.classList.add("pokemon");
 
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+
   const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
 
   const pokeInnerHTML = `
-
+    <div>
+    <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" />
+    </div>
+    <div class="info">
+      <span class="number">${pokemon.id}</span>
+      <h3 class="name">${name}</h3>
+      <small class="type">Type: <span>${type}</span></small>
+    </div>
   `;
 
   pokemonEl.innerHTML = pokeInnerHTML;
